a15d1910bcb377f2a224e185d9b172f544b04677
rosvany02 <rosvany.rodri@gmail.com>
2020-02-13 18:33:49 -0500

    First
    Hellop

---
+++ Assets/Scripts/W2Board.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W2Board : MonoBehaviour
+{
+	public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		var pos : Vector3 = transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (Input.GetKeyDown(KeyCode.A))
+		{
+			player.transform.position += new Vector3(0, 0, 1);
+
+		}
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			player.transform.position += new Vector3(0, 0, -1);
+
+		}
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			player.transform.position += new Vector3(1, 0, 0);
+
+		}
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			player.transform.position += new Vector3(-1, 0, 0);
+
+		}
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			player.transform.position += new Vector3(0, 1, 0);
+
+		}
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			player.transform.position += new Vector3(0, -1, 0);
+
+		}
+		if(Input.GetKeyDown(KeyCode.Z))
+		{
+			player.transform.position = new Vector3(7, 1, 7);
+
+		}
+	}
+}
